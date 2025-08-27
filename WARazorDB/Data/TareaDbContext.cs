b268173b8bebd171135cc0d3d135feedc23d0e28
karciss <cisneroscarolina869@gmail.com>
2025-08-26 21:02:20 -0400

    avance conexion

---
+++ WARazorDB/Data/TareaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WARazorDB.Models;
+namespace WARazorDB.Data
+{
+    public class TareaDbContext : DbContext
+    {
+        public TareaDbContext(DbContextOptions<TareaDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarea> Tareas { get; set; }
+
+        protected TareaDbContext()
+        {
+        }
+    }
+}
---
+++ WARazorDB/Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace WARazorDB.Models
+{
+    public class Tarea
+    {
+        public int idTarea { get; set; }
+        public string nombreTarea { get; set; }
+        public DateTime fechaVencimiento { get; set; }
+        public string estado { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
